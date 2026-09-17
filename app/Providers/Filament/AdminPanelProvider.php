f094d3fbb067c1fe3eb21d6fd9ae1a79972723ec
jon1267 <jonleev@gmail.com>
2026-09-17 13:53:34 +0300

    Custom Filament admin provider. Part 1

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,33 +27,19 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
-            ->colors([
-                'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
-            ->pages([
-                Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                PreventRequestForgery::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
+
+            ->brandName('Inventory Management System')
+            ->brandLogo(fn (): string => asset('images/logo.png'))
+            ->brandLogoHeight('3rem')
+            ->favicon('images/favicon.png')
+            // --- Colors ----------------
+            ->colors(['primary' => Color::Emerald,])
+            // --- Dark Mode off ---------
+            ->darkMode(false)
+            // -- SideBar-----------------
+            ->sidebarCollapsibleOnDesktop()
+            // --- NAVIGATION GROUPS ------
+            ;
+
     }
 }
